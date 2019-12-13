eeefe480c282813fcbb7eae9e3b11636556ddb3a
Yuyao Shi <yukyewshek@outlook.com>
2019-12-13 15:51:22 +0800

    rewrite giou loss based by definition

--- layers/modules/weight_GIoU_loss.py
+++ layers/modules/weight_GIoU_loss.py
@@ -13,51 +13,32 @@
 import torch
 
 
-def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False):
-    """Calculate overlap between two set of bboxes.
-
-    If ``is_aligned`` is ``False``, then calculate the ious between each bbox
-    of bboxes1 and bboxes2, otherwise the ious between each aligned pair of
-    bboxes1 and bboxes2.
-
+def iou_giou_loss(bboxes1, bboxes2):
+    """Calculate iou/giou loss between two set of bboxes.
     Args:
         bboxes1 (Tensor): shape (m, 4)
-        bboxes2 (Tensor): shape (n, 4), if is_aligned is ``True``, then m and n
-            must be equal.
-        mode (str): "iou" (intersection over union) or iof (intersection over
-            foreground).
-
+        bboxes2 (Tensor): shape (m, 4)
     Returns:
-        ious(Tensor): shape (m, n) if is_aligned == False else shape (m, 1)
+        giou loss(Tensor): shape (1, m)
     """
-
-    assert mode in ['iou', 'giou', 'iof']
 
     lt = torch.max(bboxes1[:, :2], bboxes2[:, :2])
     rb = torch.min(bboxes1[:, 2:], bboxes2[:, 2:])
 
-    wh = (rb - lt + 1).clamp(min=0)
+    wh = (rb - lt).clamp(min=0)  # 卡住wh的最小值为0(当两者无相交部分时,设定交集为0)
+
     overlap = wh[:, 0] * wh[:, 1]
-    area1 = (bboxes1[:, 2] - bboxes1[:, 0] + 1) * (
-            bboxes1[:, 3] - bboxes1[:, 1] + 1)
 
-    if mode == 'iou':
-        area2 = (bboxes2[:, 2] - bboxes2[:, 0] + 1) * (
-                bboxes2[:, 3] - bboxes2[:, 1] + 1)
-        ious = overlap / (area1 + area2 - overlap)
-        return ious
-    elif mode == 'giou':
-        area2 = (bboxes2[:, 2] - bboxes2[:, 0] + 1) * (
-                bboxes2[:, 3] - bboxes2[:, 1] + 1)
-        clt = torch.min(bboxes1[:, :2], bboxes2[:, :2])
-        crb = torch.max(bboxes1[:, 2:], bboxes2[:, 2:])
+    area_a = (bboxes1[:, 2] - bboxes1[:, 0]) * (bboxes1[:, 3] - bboxes1[:, 1])
+    area_b = (bboxes2[:, 2] - bboxes2[:, 0]) * (bboxes2[:, 3] - bboxes2[:, 1])
 
-        area_c = (crb[:, 0] - clt[:, 0] + 1) * (
-                crb[:, 1] - clt[:, 1] + 1)
-        U = (area1 + area2 - overlap)
-        ious = overlap / U
-        gious = ious - (area_c - U) / area_c
-        return gious
-    else:
-        ious = overlap / area1
-        return ious
+    iou = overlap / (area_a + area_b - overlap)
+
+    area_c_lt = torch.min(bboxes1[:, :2], bboxes2[:, :2])
+    area_c_rb = torch.max(bboxes1[:, 2:], bboxes2[:, 2:])
+
+    area_c = (area_c_rb[:, 0] - area_c_lt[:, 0]) * (area_c_rb[:, 1] - area_c_lt[:, 1])
+    U = (area_a + area_b - overlap)
+    giou = iou - (area_c - U) / area_c
+
+    return 1 - giou
