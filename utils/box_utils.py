6f412ca41e0d8617e5237158c757ae7384afeb46
Yuyao Shi <yukyewshek@outlook.com>
2019-12-13 15:51:48 +0800

    standard code

--- utils/box_utils.py
+++ utils/box_utils.py
@@ -204,7 +204,6 @@ def match(threshold, truths, priors, variances, labels, loc_t, conf_t, idx):
     matches = truths[best_truth_idx]  # Shape: [num_priors,4]
     conf = labels[best_truth_idx]  # Shape: [num_priors]
     conf[best_truth_overlap < threshold] = 0  # label as background
-    # conf[:] = 1
     loc = encode(matches, priors, variances)
     loc_t[idx] = loc  # [num_priors,4] encoded offsets to learn
     conf_t[idx] = conf  # [num_priors] top class label for each prior
